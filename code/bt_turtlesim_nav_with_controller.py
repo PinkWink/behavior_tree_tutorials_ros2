72df0634f14274f336c5abd44b5044d2acfd0af5
PinkWink <pinkwink.korea@gmail.com>
2024-11-03 15:09:40 +0900

    add_controller

--- code/bt_turtlesim_nav_with_controller.py
+++ code/bt_turtlesim_nav_with_controller.py
@@ -1,19 +1,19 @@
 import py_trees
 import py_trees_ros
 import rclpy
-
-from py_trees.behaviour import Behaviour
-from py_trees.common import Status
 from py_trees.decorators import Inverter
 from py_trees import logging as log_tree
 from my_behavior_tree_msgs.msg import PosError
-from rclpy.node import Node
-from geometry_msgs.msg import Twist
-from my_behavior_tree.bt_turtlesim_nav_classes import CheckTargetInRange, CheckDistanceErrorNegative
-from my_behavior_tree.bt_turtlesim_nav_classes import CheckAngleInRange, ActionDummy
+from my_behavior_tree.bt_turtlesim_nav_classes_with_controller import ActionRotateForward
+from my_behavior_tree.bt_turtlesim_nav_classes_with_controller import CheckTargetInRange
+from my_behavior_tree.bt_turtlesim_nav_classes_with_controller import CheckAngleInRange
+from my_behavior_tree.bt_turtlesim_nav_classes_with_controller import ActionRotateToTargetDirection
+from my_behavior_tree.bt_turtlesim_nav_classes_with_controller import CheckDistanceErrorNegative
+from my_behavior_tree.bt_turtlesim_nav_classes_with_controller import ActionRotateReverse
+from my_behavior_tree.bt_turtlesim_nav_classes_with_controller import ActionMoveForward
 
 def move_forward_seq_mode():
-    move_forward = ActionDummy('action_move_forward') 
+    move_forward = ActionMoveForward('action_move_forward') 
 
     move_forward_seq = py_trees.composites.Sequence(name="move_forward_seq", memory=True)
     move_forward_seq.add_child(move_forward)
@@ -21,8 +21,8 @@ def move_forward_seq_mode():
     return move_forward_seq
 
 def arrival_check_seq_mode():
-    is_dist_in_range_2 = CheckTargetInRange('check_target_in_range')
-    rotate_to_target_direction = ActionDummy('action_rotate_to_target_direction')
+    is_dist_in_range_2 = CheckTargetInRange('check_target_in_range_2')
+    rotate_to_target_direction = ActionRotateToTargetDirection('action_rotate_to_target_direction')
 
     arrival_check_seq = py_trees.composites.Sequence(name="arrival_check_seq", memory=True)
     arrival_check_seq.add_children([is_dist_in_range_2, rotate_to_target_direction])
@@ -35,7 +35,7 @@ def misaligned_target_sel_mode():
     is_dist_in_range_3 = CheckTargetInRange('is_dist_in_range_3')
     is_not_dist_in_range = Inverter(name="is_not_dist_in_range", child=is_dist_in_range_3)
 
-    rotate_forward = ActionDummy('action_rotate_forward')
+    rotate_forward = ActionRotateForward('action_rotate_forward')
 
     misaligned_target_sel = py_trees.composites.Sequence(name="misaligned_target_sel", memory=True)
     misaligned_target_sel.add_children([is_not_angle_in_range, is_not_dist_in_range, rotate_forward])
@@ -69,9 +69,9 @@ def reverse_target_seq_mode():
         qos_profile=py_trees_ros.utilities.qos_profile_unlatched(),
     )
     is_dist_err_neg = CheckDistanceErrorNegative('check_distance_error_negative') 
-    is_dist_in_range_1 = CheckTargetInRange('check_target_in_range')
+    is_dist_in_range_1 = CheckTargetInRange('check_target_in_range_1')
     is_not_dist_in_range = Inverter(name="is_not_dist_in_range", child=is_dist_in_range_1)
-    rotate_to_reverse = ActionDummy('action_rotate_reverse')
+    rotate_to_reverse = ActionRotateReverse('action_rotate_reverse')
 
     reverse_target_seq = py_trees.composites.Sequence(name="reverse_target_seq", memory=True)
     reverse_target_seq.add_children([get_errors, is_dist_err_neg, is_not_dist_in_range, rotate_to_reverse])
